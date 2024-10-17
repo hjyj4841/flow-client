dfc032f7c031c7504577c10ec2acebe32a54aa6f
soneuron <ardor00@naver.com>
2024-10-17 09:14:41 +0900

    fix  : report 관련

--- src/api/report.js
+++ src/api/report.js
@@ -1,31 +1,70 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:8080/api/",
-});
-
+import {
+  getReportPost,
+  deletePost,
+  getReportUser,
+  deleteUser,
+  banUser,
+  reportPost,
+  reportUser,
+} from "../api/report";
+export const initState = {
+  reportPosts: [],
+};
+export const rUserState = {
+  reportUsers: [],
+};
+// 액션함수
 // 신고받은 글 가져오기
-export const getReportPost = async () => {
-  return await instance.get("showAllPostReport");
+export const fetchReportPost = async (dispatch) => {
+  const response = await getReportPost();
+  dispatch({ type: "FETCH_REPORT_POST", payload: response.data });
 };
-
-export const deletePost = async (postReportCode) => {
-  return await instance.delete(
-    "delPostReport?postReportCode=" + postReportCode
-  );
+// 신고글 지우기
+export const fetchDeleteReportPost = async (dispatch, postReportCode) => {
+  await deletePost(postReportCode);
+  dispatch({ type: "FETCH_DELETE_REPORT_POST" });
 };
-
-export const reportPost = async () => {
-  return await instance.post("reportPost");
+// 유저 신고 가져오기
+export const fetchReportUser = async (dispatch) => {
+  const response = await getReportUser();
+  dispatch({ type: "FETCH_REPORT_USER", payload: response.data });
 };
-
-export const getReportUser = async () => {
-  return await instance.get("showAllUserReport");
+// 유저 신고 삭제하기
+export const delReportUser = async (dispatch, userReportCode) => {
+  await deleteUser(userReportCode);
+  dispatch({ type: "DELETE_REPORT_USER" });
 };
-
-// 수정필요 => 삭제하면서 밴도 해야함
-export const deleteUser = async (userReportCode) => {
-  return await instance.delete(
-    "delUserReport?userReportCode=" + userReportCode
-  );
+// 유저 밴하기
+export const banUserReport = async (dispatch, userCode) => {
+  await banUser(userCode);
+  dispatch({ type: "BAN_USER_REPORT" });
+};
+export const addReportPost = async (dispatch, data) => {
+  await reportPost(data);
+  dispatch({ type: "REPORT_POST" });
+};
+export const addReportUser = async (dispatch, data) => {
+  await reportUser(data);
+  dispatch({ type: "REPORT_USER" });
+};
+export const reportReducer = (state, action) => {
+  switch (action.type) {
+    // 신고받은 글 가져오기
+    case "FETCH_REPORT_POST":
+      return { ...state, reportPosts: action.payload };
+    case "FETCH_DELETE_REPORT_POST":
+      return state;
+    case "FETCH_REPORT_USER":
+      return { ...state, reportUsers: action.payload };
+    case "DELETE_REPORT_USER":
+      return state;
+    case "BAN_USER_REPORT":
+      return state;
+    case "REPORT_POST":
+      return state;
+    case "REPORT_USER":
+      return state;
+    default:
+      return state;
+  }
 };
