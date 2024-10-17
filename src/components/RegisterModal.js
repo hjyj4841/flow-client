d7af7e62f78d2577473c7cfe0c78d57fd10d7de4
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-10-17 09:38:45 +0900

    fix : modal 추가

---
+++ src/components/RegisterModal.js
@@ -0,0 +1,67 @@
+import { useRef, useState } from "react";
+import { getKakaoCode } from "../api/kakao";
+import { getGoogleCode } from "../api/google";
+import { getNaverCode } from "../api/naver";
+import "../assets/css/header.css";
+const RegisterModal = () => {
+  const [registerOpen, setRegisterOpen] = useState(false);
+  const registerBackground = useRef();
+  return (
+    <>
+      <button
+        className={"register-open-btn"}
+        onClick={() => setRegisterOpen(true)}
+      >
+        회원가입
+      </button>
+      {registerOpen && (
+        <div
+          className={"register-container"}
+          ref={registerBackground}
+          onClick={(e) => {
+            if (e.target === registerBackground.current) {
+              setRegisterOpen(false);
+            }
+          }}
+        >
+          <div className={"register-content"}>
+            <h1>회원가입</h1>
+            <button
+              type="button"
+              className="google"
+              onClick={() => getGoogleCode("register")}
+            >
+              <span className="blue">G</span>
+              <span className="red">o</span>
+              <span className="yellow">o</span>
+              <span className="blue">g</span>
+              <span className="green">l</span>
+              <span className="red">e</span>
+            </button>
+            <button
+              type="button"
+              className="kakao"
+              onClick={() => getKakaoCode("register")}
+            >
+              Kakao
+            </button>
+            <button
+              type="button"
+              className="naver"
+              onClick={() => getNaverCode("register")}
+            >
+              NAVER
+            </button>
+            <button
+              className={"login-close-btn"}
+              onClick={() => setRegisterOpen(false)}
+            >
+              닫기
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+export default RegisterModal;
