e7182c5f8bf219923d9fc50a0640c25205c374ff
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2024-10-25 17:48:19 +0900

    fix: 추합 후 수정

--- src/pages/post/PopularFeed.js
+++ src/pages/post/PopularFeed.js
@@ -120,7 +120,7 @@ const PopularFeed = () => {
                 />
                 <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity">
                   <p
-                    className="text-white cursor-pointer"
+                    className="text-white cursor-pointer popular-p"
                     onClick={() => detail(post.postCode)}
                   >
                     {post.postDesc}
