cba2938ada914caf6425270573af24d8532ff09c
juy0210 <juy0210@gmail.com>
2024-10-30 17:31:25 +0900

    feat : 대댓글 작성 구현중

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -12,59 +12,7 @@ const Comment = ({ comment, postCode }) => {
     commentDesc: "",
     postCode: postCode,
     user: user,
-    parentCommentCode: 0,
+    parentCommentCode: comment.commentCode || 0,
   });
-
-  // 댓글 작성
-  const addMutation = useMutation({
-    mutationFn: addComment,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["comments", postCode] });
-    },
-  });
-
-  // 댓글 수정
-  const updateMutation = useMutation({
-    mutationFn: updateComment({ ...comment, commentDesc }),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["comments", postCode] });
-      // setIsUpdating(false);
-    },
-  });
-
-  // 댓글 삭제
-  const deleteMutation = useMutation({
-    mutationFn: (commentCode) => deleteComment(commentCode),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["comments", postCode] });
-    },
-  });
-
-  const handleUpdate = () => {
-    updateMutation.mutate({ ...comment, commentDesc });
-  };
-
-  const handleUpdateCancel = () => {
-    setNewComment({ ...newComment, commentDesc: "", commentCode: 0 });
-  };
-
-  const handleDelete = () => {
-    deleteMutation.mutate(comment.commentCode);
-  };
-
-  return;
-  <>
-    {user.commentCode === comment.commentCode && (
-      <button
-        type="text"
-        value={newComment.commentDesc}
-        onClick={() => handleUpdate(comment.commentDesc)}
-        className="text-black px-1 hover:text-gray-900 text-gray-500"
-      >
-        수정
-      </button>
-    )}
-  </>;
 };
-
 export default Comment;
